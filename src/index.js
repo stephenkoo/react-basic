453e29ea26c301a155257273d95e82d7fa286962
Stephen Koo <stephenyrkoo@gmail.com>
2017-09-27 21:38:29 +1000

    Destructure ReactDOM render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import 'styles/main.css';
-import App from 'components/App';
+import App from 'App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
